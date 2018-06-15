6062d1edea0ec40f4fd101eabeaf8f04c0a737d0
Takumi Ishiyama <mt.stone20@gmail.com>
2018-06-16 04:05:51 +0900

    [add] japanese_input.gif

--- EditAreaListener.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.event.*;
-
-interface EditAreaListener extends DocumentListener {
-  public void handleShareInsert(DocumentEvent e);
-  public void handleShareRemove(DocumentEvent e);
-  public void insertUpdate(DocumentEvent e);
-  public void removeUpdate(DocumentEvent e);
-  public void changedUpdate(DocumentEvent e);
-}
